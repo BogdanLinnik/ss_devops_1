5e223c3ca42a4f1df4d3cdd891484388456709ec
Bogdan Linnik <bogdan@geologie.com>
2025-01-10 20:10:26 +0200

    Add logs about script execution start and finish

--- bash.sh
+++ bash.sh
@@ -1,7 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
+echo "Start counting"
+
 COUNTER=0 
 while [ $COUNTER -lt 10 ] 
 do 
    echo The counter is $COUNTER 
    let COUNTER=$COUNTER+1 
 done
+
+echo "Finish counting"
